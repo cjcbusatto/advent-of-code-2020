ee771c305ed6140280e2c3ce931e6cad7dd421d0
Claudio Busatto <cjcbusatto@gmail.com>
2020-12-06 21:55:37 +0100

    reduce number of comparisons

--- 01/main.py
+++ 01/main.py
@@ -10,19 +10,23 @@ def get_expenses_from_input(input_location):
     return expenses_list_number
 
 def get_two_expenses_which_sum_2020(expenses):
-    for i, _ in enumerate(expenses):        
-        for j, __ in enumerate(expenses):
-            inverted_index = len(expenses) - j - 1
-            if(expenses[i] + expenses[inverted_index] > 2020):
-                continue
+    counter = 0
+    for i,_ in enumerate(expenses):        
+        for j,__ in enumerate(expenses):
+            counter+=1
+            print(f"i={i}, j={j} = {expenses[i] + expenses[j]}")
+            if(expenses[i] + expenses[j] > 2020):
+                break
 
-            if(expenses[i] + expenses[inverted_index] == 2020):
-                return (expenses[i], expenses[inverted_index])
+            if(expenses[i] + expenses[j] == 2020):
+                print(f"Number of comparisons: {counter}")
+                return (expenses[i], expenses[j])
     
     return "Error"
 
 expenses = get_expenses_from_input('input.txt')
 
-value1,value2 = get_two_expenses_which_sum_2020(expenses)
+value1, value2 = get_two_expenses_which_sum_2020(expenses)
+print(f"value1={value1}, value2={value2}")
 result = value1 * value2
-print(result)
+print(f"value1 x value2 = {result}")
